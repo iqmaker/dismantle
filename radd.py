9a862542aeb40b0c1c73e7f65c6ef4c8779400b9
sbr <iqmaker@gmail.com>
2011-08-02 10:54:52 +0400

    radd beauty

--- radd.py
+++ radd.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import os
 extensions = [ 'html', 'py', 'js' ]
 for e in extensions:
     command = 'find -name "*.%s" | xargs git add' % e
-    print command
     os.system( command )
